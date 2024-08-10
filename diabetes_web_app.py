e524f4f96d0e46bc120dcd80d6dfa22ce256e021
@Mikist <mikeochango3@gmail.com>
2024-08-10 23:42:31 +0300

    Difine the input  data function

--- diabetes_web_app.py
+++ diabetes_web_app.py
@@ -5,3 +5,25 @@ import pickle
 loaded_model = pickle.load(open("C:/Users/User/Documents/codebasics/diabetes_ml_project/trained_model.sav",'rb'))
 
 # creating a function for predcion
+def diabetes_prediction(input_data):
+
+
+
+    
+    
+    # Changing the data to numpy array
+    input_data_as_np_array = np.asarray(input_data)
+
+    # Reshape the array predicting one instance
+    input_data_reshaped = input_data_as_np_array.reshape(1,-1)
+
+
+    # Prediction
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    
+    if prediction[0] == 1:
+        return 'The person is diabetic'
+    else:
+        return 'The person is non diabetic'
